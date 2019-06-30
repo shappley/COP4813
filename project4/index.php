bff4ef7a2fc73c32b6e172c465c645b074d6c5b5
Kyle Shappley <Kyle.Shappley@gmail.com>
2019-06-30 14:02:46 -0400

    added index config

--- project4/index.php
+++ project4/index.php
@@ -1,16 +1,60 @@
-<?php session_start(); ?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Index</title>
-</head>
-<body>
-<h2>Hello, <?php echo $_SESSION["username"]; ?></h2>
-</body>
-</html>
-
 <?php
 //http://www.omdbapi.com/?i=tt3896198&apikey=178bb728
-?>
+session_start();
+//require_once('/home/common/mail.php');
+processPageRequest();
+
+function addMovieToCart($movieID)
+{
+    $arr = readMovieData();
+    array_push($arr, $movieID);
+    writeMovieData($arr);
+    displayCart();
+}
+
+function checkout($name, $address)
+{
+    //TODO
+}
+
+function displayCart()
+{
+    $movies = readMovieData();
+    require_once("./templates/cart_form.php");
+}
+
+function processPageRequest()
+{
+    if (isset($_GET["action"]) && !empty($_GET["action"])) {
+        $action = $_GET["action"];
+        if ($action === "add") {
+            addMovieToCart($_GET["movie_id"]);
+        } else if ($action === "remove") {
+            removeMovieFromCart($_GET["movie_id"]);
+        } else if ($action === "checkout") {
+            checkout($_SESSION["username"], $_SESSION["email"]);
+        }
+    } else {
+        displayCart();
+    }
+}
+
+function readMovieData()
+{
+    $csv = array_map('str_getcsv', file('./data/cart.db'));
+    return $csv[0];
+}
+
+function removeMovieFromCart($movieID)
+{
+    $array = readMovieData();
+    $diff = array_diff($array, [$movieID]);
+    writeMovieData($diff);
+    displayCart();
+}
+
+function writeMovieData($array)
+{
+    $csv = implode(",", $array);
+    file_put_contents("./data/cart.db", $csv);
+}
